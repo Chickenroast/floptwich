1b0360eb8df5d2bede51e146f6e0ebaa581f4585
Julia Michel T. <juliamicheltex@gmail.com>
2023-10-27 23:46:36 +0200

    begin

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,17 @@
+import laravelVitePlugin from "laravel-vite-plugin";
 import { defineConfig } from "vite";
+import path from "path"; // Ajoutez cette ligne pour importer le module path
 
 export default defineConfig({
   base: "/floptwich/",
+  root: path.resolve(__dirname, "src"),
+  resolve: {
+    alias: {
+      "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+    },
+  },
+  server: {
+    port: 8080,
+    hot: true,
+  },
 });
