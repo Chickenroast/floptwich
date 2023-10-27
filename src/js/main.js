61d445b091b229f64287bf0f9cb955bee79bbffd
Julia Michel T. <juliamicheltex@gmail.com>
2023-10-27 23:57:56 +0200

    noooo

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,17 @@
 import "../scss/styles.scss";
 import * as bootstrap from "bootstrap";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const drumPads = document.querySelectorAll(".drum-pad");
+  const audio = document.getElementById("audio");
+
+  drumPads.forEach((pad) => {
+    pad.addEventListener("click", playSound);
+  });
+
+  function playSound() {
+    const soundFile = this.getAttribute("data-sound");
+    audio.src = soundFile;
+    audio.play();
+  }
+});
